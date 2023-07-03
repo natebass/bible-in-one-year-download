d36e851406815ec560854645734865bd15fae0fc
Nate Bass <nw3455@gmail.om>
2023-07-02 19:42:05 -0700

    WIP 01: Refactor to classes

--- main.py
+++ main.py
@@ -1,35 +1,92 @@
-import sys
+from os import path, mkdir, remove, listdir
+from pathlib import Path
 
-from RSSParser import download_episode
+from rssmp3parser.CommandLineArguments import CommandLineArguments
+from rssmp3parser.RSSMp3Downloader import RSSMp3Downloader
+from rssmp3parser.RSSParser import RSSParser
+from rssmp3parser.RSSType import parse_rss_type
+
+
+def print_error(error_message):
+    """
+    Print cli errors, library exceptions, and other error messages to the console.
+    :parameter str error_message: The error message text
+    """
+    print(error_message)
+
+
+def create_directory(directory):
+    """
+    Check if valid directory and create if it doesn't already exist.
+    :parameter Path directory: The error message text
+    """
+    if not path.exists(directory):
+        mkdir(directory)
+
+
+def parse_arguments():
+    """
+    Parse arguments passed to the CLI. Configure an RSSParser during this process.
+    :return RSSParser: A class that fetches and parses the required RSS file.
+    """
+    arguments = CommandLineArguments()
+    rss_type = parse_rss_type('')
+    # if (arguments_count := len(sys.argv)) < 2:
+    #     print_error('This script requires at least one argument.')
+    #     raise SystemExit(2)
+    # argument1 = sys.argv[1]
+    # argument2 = sys.argv[2]
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--bar', help='Do the bar option')
+    # parser.add_argument('--foo', help='Foo the program')
+    # args = parser.parse_args()
+    # print(f'Args: {args}\nCommand Line: {sys.argv}\nfoo: {args.foo}')
+    # print(f'Dict format: {vars(args)}')
+    arguments.audio_files_directory = Path(__file__).parent / 'Audio Files'
+    arguments.feed_url = Path(__file__).parent / f'{rss_type}.xml'
+    return arguments, rss_type
 
 
 def main():
     """
-    Python script to be run in the console.
-   :argument str episode_number: The episode to be downloaded
-   :argument str rss_file: Path of local RSS file to be used offline
+    Download Bible in One Year episodes. This script is meant to be run through a command line.
+    1. cli-argument str episode_number: The episode to be downloaded.
+    2. cli-argument str rss_file: Either the URL or the file path of a local RSS file.
+    * --cache-enabled bool: Specify whether to cache RSS file information
     """
-    print(*sys.argv)
-    if len(sys.argv) < 2:
-        print("This script requires at least one argument.")
-    argument1 = sys.argv[1]
-    argument2 = sys.argv[2]
-    download_episode(argument1, argument2)
+    # 1. Create an RSSParser based on the arguments passed to the CLI
+    arguments, rss_type = parse_arguments()
+    # 2. Parse RSS file
+    rss_parser = RSSParser(rss_type=rss_type, feed_xml=arguments.feed_url)
+    # 2. Handle caching data about the RSS file
+    create_directory(arguments.audio_files_directory)
+    if arguments.cache_enabled:
+        rss_parser.save_rss_as_file(Path.cwd())
+    elif arguments.remove_cache:
+        for file in listdir('.'):
+            if file.endswith('.xml') or file.endswith('.rss') or file == rss_parser.config_file:
+                remove(file)
+    # 3. Download the files
+    downloader = RSSMp3Downloader(mp3_files=rss_parser.get_mp3_files(), episode_number=arguments.episode,
+                                  audio_files_directory=arguments.audio_files_directory)
+    if arguments.download_all:
+        try:
+            downloader.download_all_episodes()
+        except Exception as exception:
+            print_error(str(exception))
+    else:
+        try:
+            downloader.download_episode(arguments.episode)
+        except Exception as exception:
+            print_error(str(exception))
 
 
 if __name__ == '__main__':
     main()
 
-# audio_files_folder = Path.joinpath(Path(__file__).parent, "Audio Files")
-# if not Path.exists(audio_files_folder):
-#     os.mkdir(audio_files_folder)
-#
-# entries = parse_bioy_feed_for_mp3(requests.get(rss_feed).text)
-# for audio_file_link in entries:
-#     response = requests.get(audio_file_link)
-#     mp3_file = Path.joinpath(audio_files_folder, Path(unquote(urlparse(audio_file_link).path)).parts[-1])
-#     if not Path.exists(mp3_file):
-#         with open(mp3_file, "wb") as file:
-#             file.write(response.content)
-#     raise "a"
-#
+# else:
+#     print_error('Please specify and episode.')
+#     raise SystemExit(2)
+# if next(glob.iglob('*.xml'), False):
+#     raise Exception('There is an xml file in your current directory. Would you like to use that next time by '
+#                     'running main.py -f <rss_file>?')
